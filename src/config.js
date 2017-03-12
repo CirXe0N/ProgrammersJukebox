961f276229268c05ce832f0eaf8f9f0c4282e1f9
CirXe0N <dw1ls@outlook.com>
2017-03-12 12:54:16 +0100

    Update Hip Hop playlist

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const SOUNDCLOUD_PLAYLIST = {
     'HOUSE': 'https://soundcloud.com/louis-v-d-linden/sets/house',
     'CHILL': 'https://soundcloud.com/jim-gares/sets/chill',
     'DEEP': 'https://soundcloud.com/deephousespace/sets/deep-house',
-    'HIP HOP': 'https://soundcloud.com/mathieu-sorlin/sets/new-style-hip-hop',
+    'HIP HOP': 'https://soundcloud.com/dazeddubz/sets/hip-hop',
     'DUBSTEP': 'https://soundcloud.com/skrillex93/sets/dubstep',
     'TECH': 'https://soundcloud.com/gaz-manchester/sets/essential-tech',
 };
